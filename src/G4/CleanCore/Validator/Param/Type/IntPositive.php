4eabacf1ac2e3d39d8193efc1ff94c7f86756911
Drasko Gomboc <drasko.gomboc@gmail.com>
2013-12-23 17:59:40 +0100

    int positive type value check

--- src/G4/CleanCore/Validator/Param/Type/IntPositive.php
+++ src/G4/CleanCore/Validator/Param/Type/IntPositive.php
@@ -9,6 +9,6 @@ class IntPositive extends \G4\CleanCore\Validator\Param\Type\Int
 
     public function type()
     {
-        return parent::type() && $this->_value < 0;
+        return parent::type() && $this->_value > 0;
     }
 }
