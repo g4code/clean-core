ff568892d3b7336d7519849d8ad24e34dc42f276
Drasko Gomboc <drasko.gomboc@gmail.com>
2014-01-13 18:13:44 +0100

    UseCase Factory; useCaseReference method in FormatterAbstract

---
+++ src/G4/CleanCore/Factory/UseCase.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace G4\CleanCore\Factory;
+
+use G4\CleanCore\Response\Response;
+use G4\CleanCore\Request\Request;
+
+class UseCase
+{
+
+    /**
+     * @var \G4\CleanCore\Request\Request
+     */
+    private $_request;
+
+    /**
+     * @var \G4\CleanCore\Response\Response
+     */
+    private $_response;
+
+    /**
+     * @var \G4\CleanCore\UseCase\UseCaseAbstract
+     */
+    private $_useCase;
+
+    /**
+     * @var string
+     */
+    private $_useCaseName;
+
+
+    /**
+     * @throws \Exception
+     * @return \G4\CleanCore\Request\Request
+     */
+    public function getRequest()
+    {
+        if (!$this->_request instanceof \G4\CleanCore\Request\Request) {
+            throw new \Exception('Request must be instance of \G4\CleanCore\Request\Request');
+        }
+        return $this->_request;
+    }
+
+    /**
+     * @param string $part
+     * @return mixed
+     */
+    public function getResource($part = null)
+    {
+        return $part === null
+            ? $this->getResponse()->getResponseObject()
+            : $this->getResponse()->getResponseObjectPart($part);
+    }
+
+    /**
+     * @return G4\CleanCore\Response\Response
+     */
+    public function getResponse()
+    {
+        if (!$this->_response instanceof \G4\CleanCore\Response\Response) {
+            $this->_setResponse();
+        }
+        return $this->_response;
+    }
+
+    /**
+     * @throws \Exception
+     * @return \G4\CleanCore\UseCase\UseCaseAbstract
+     */
+    public function getUseCase()
+    {
+        if (!$this->_useCase instanceof \G4\CleanCore\UseCase\UseCaseAbstract) {
+            $this->_factory();
+        }
+        return $this->_useCase;
+    }
+
+    /**
+     * @param \G4\CleanCore\Request\Request $request
+     * @return \G4\CleanCore\Factory\UseCase
+     */
+    public function setRequest(Request $request)
+    {
+        $this->_request = $request;
+        return $this;
+    }
+
+    /**
+     * @param string $useCaseName
+     * @return \G4\CleanCore\Factory\UseCase
+     */
+    public function setUseCaseName($useCaseName)
+    {
+        $this->_useCaseName = $useCaseName;
+        return $this;
+    }
+
+    /**
+     * @return \G4\CleanCore\Factory\UseCase
+     */
+    private function _factory()
+    {
+        $useCaseName    = $this->_useCaseName;
+        $this->_useCase = new $useCaseName();
+        $this->_useCase
+            ->setRequest($this->getRequest());
+        return $this;
+    }
+
+    /**
+     * @return \G4\CleanCore\Factory\UseCase
+     */
+    private function _setResponse()
+    {
+        $this->getUseCase()->run();
+        $this->_response = $this->getUseCase()->getResponse();
+        return $this;
+    }
+}
--- src/G4/CleanCore/Formatter/FormatterAbstract.php
+++ src/G4/CleanCore/Formatter/FormatterAbstract.php
@@ -3,6 +3,7 @@
 namespace G4\CleanCore\Formatter;
 
 use G4\CleanCore\Formatter\FormatterInterface;
+use G4\CleanCore\Request\Request;
 
 abstract class FormatterAbstract implements FormatterInterface
 {
@@ -12,6 +13,26 @@ abstract class FormatterAbstract implements FormatterInterface
      */
     protected $_resource;
 
+
+    /**
+     * @param string $part
+     * @return mixed
+     */
+    public function getResource($part = null)
+    {
+        return $part === null
+            ? $this->_resource
+            : $this->_getResourcePart($part);
+    }
+
+    /**
+     * @return \G4\CleanCore\Factory\UseCase
+     */
+    public function getUseCaseFactoryInstance()
+    {
+        return new \G4\CleanCore\Factory\UseCase();
+    }
+
     /**
      * @param mixed $response
      * @return FormatterAbstract
@@ -20,5 +41,30 @@ abstract class FormatterAbstract implements FormatterInterface
     {
         $this->_resource = $resource;
         return $this;
+    }
+
+    /**
+     * @param string $useCaseName
+     * @param string $resourcePart
+     * @param \G4\CleanCore\Request\Request $request
+     * @return mixed
+     */
+    public function useCaseReference($useCaseName, $resourcePart = null, Request $request = null)
+    {
+        return $this->getUseCaseFactoryInstance()
+            ->setUseCaseName($useCaseName)
+            ->setRequest($request)
+            ->getResource($resourcePart);
+    }
+
+    /**
+     * @param string $part
+     * @return mixed
+     */
+    private function _getResourcePart($part)
+    {
+        return isset($this->_resource[$part])
+            ? $this->_resource[$part]
+            : null;
     }
 }
