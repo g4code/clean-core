3faf2b71796bd64e293dcf99097a64851c55487d
Petar Pavlovic <pavlovicpetar@gmail.com>
2018-07-18 11:00:41 +0200

    Replace space ' ' in email address with plus '+'

--- src/Validator/Param/Type/Email.php
+++ src/Validator/Param/Type/Email.php
@@ -6,6 +6,7 @@ class Email extends StringValidator
 {
     public function type()
     {
+        $this->_value = preg_replace('/\s+/', '+', trim($this->_value));
         return parent::type()
             && filter_var($this->_value, FILTER_VALIDATE_EMAIL);
     }
