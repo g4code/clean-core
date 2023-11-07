f93e47483a0abfe16e00839cfc62956cd1c3a560
Mirko Bukilic <mirko.bukilic@codeplicity.com>
2023-11-07 09:19:49 +0100

    Make package PHP8.2 compatible

--- src/Validator/Param/ParamAbstract.php
+++ src/Validator/Param/ParamAbstract.php
@@ -7,21 +7,18 @@ use G4\CleanCore\Request\Request;
 abstract class ParamAbstract implements ParamInterface
 {
     //TODO: Drasko: should be private!
-
     protected $value;
 
     /**
+     *  Possible options:
+     *      default
+     *      required
+     *      separator
+     *      type
+     *      valid
      * @param mixed[] $meta
      */
-    public function __construct(protected $name, private readonly Request $request, /**
-     * Possible options:
-     *     default
-     *     required
-     *     separator
-     *     type
-     *     valid
-     */
-    protected $meta)
+    public function __construct(protected $name, private readonly Request $request, protected $meta)
     {
         $this->value  = $request->getParam($name);
     }
