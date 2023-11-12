a2a55f3538772e4ab08fb3738dfd77a4d20bb7c9
Petar Pavlović <pavlovicpetar@gmail.com>
2023-11-12 12:10:45 +0100

    Fix 8.2 notice \DateTime::createFromFormat second parameter could not be null

--- src/Validator/Param/Type/Date.php
+++ src/Validator/Param/Type/Date.php
@@ -6,9 +6,9 @@ class Date extends StringValidator
 {
     public function isInValidRange(): bool
     {
-        $date = \DateTime::createFromFormat($this->meta['valid'], $this->value);
+        $date = \DateTime::createFromFormat($this->meta['valid'], $this->value ?: '');
 
         return $date !== false
-            && $date->format($this->meta['valid']) == $this->value;
+            && $date->format($this->meta['valid']) === $this->value;
     }
 }
